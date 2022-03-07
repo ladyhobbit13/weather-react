949a291ab74a9cd9c65a0420012d79be519c95de
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-07 10:56:20 -0800

    Updated Weather.js
    Added non-functional pieces

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,45 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-  return <h2>Hello from Weather</h2>;
+  return (
+    <div className="buttons">
+      <form id="search-form" className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          id="city-search"
+          aria-describedby="emailHelp"
+          placeholder="Where Will You Be Walking?"
+        />
+        <button type="submit" className="btn btn-primary" id="search-button">
+          👟👟
+        </button>
+      </form>
+      <button id="current-city">Current City</button>
+      <div className="card-body">
+        <h3 id="city-name"></h3>
+        <img src="" alt="" id="icon" />
+        <p className="card-text">
+          <ul>
+            <li>
+              <span id="temperature"></span>
+              <a href="#" id="fahrenheit-link" className="active">
+                °F
+              </a>
+              |
+              <a href="#" id="celsius-link">
+                °C
+              </a>
+            </li>
+            <li>
+              <span id="wind"></span>
+            </li>
+          </ul>
+        </p>
+      </div>
+      <br />
+      <br />
+      <div className="weather-forecast" id="forecast"></div>
+    </div>
+  );
 }
