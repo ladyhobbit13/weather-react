9311c2f606d6aeb83aef154473716fafd5051a20
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-07 11:19:21 -0800

    Update CSS

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import "./Weather.css";
 function App() {
   return (
     <div className="App">
-      <div className="container" id="weaather-app">
+      <div className="container" id="weather-app">
         <h1>
           Is It a Beautiful Day for a Walk? <em className="asterisk">*</em>
         </h1>
         <h3 id="today-date"></h3>
         <h3 id="today-time"></h3>
         <Weather />
-        <footer>
-          <a id="coded-by" href="https://github.com/ladyhobbit13/weather-react">
-            Open-source code
-          </a>
-          , by Claire Fleischer
-        </footer>
       </div>
+      <footer>
+        <a id="coded-by" href="https://github.com/ladyhobbit13/weather-react">
+          Open-source code
+        </a>
+        , by Claire Fleischer
+      </footer>
+      v
     </div>
   );
 }
